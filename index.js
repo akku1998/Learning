32e18b045065eeb3d6a8e4cc5d13933b361e088f
Akanksha Mohanty <38135774+akku1998@users.noreply.github.com>
2019-02-06 01:16:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+
+Index.js
+
+*/
+
+	var msg="Hello Javascript";
+	console.log(msg);
+
+	var resultsDiv = document.getElementById("result");
+	resultsDiv.innerHTML = "<p>This is from Javascript</p>";
+
+	console.log("msg is "+ typeof(msg));
+	console.log("resultsDiv is " + typeof(resultsDiv));
+
+	var none;
+	console.log("none is " + typeof(none));
+
+	var aNumber = 0;
+	console.log("aNumber is " + typeof(aNumber));
+
+	var trueFalse = true;
+	console.log("trueFalse is " + typeof(trueFalse));
+
